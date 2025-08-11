d82fcbf437d86eb23932b9864bc049294d12e415
trevor1235 <fsseruwugge@gmail.com>
2025-08-11 15:21:44 +0100

    Committing latest changes

--- core/urls.py
+++ core/urls.py
@@ -18,14 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from builder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('', include('builder.urls')),
 ]
 
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
